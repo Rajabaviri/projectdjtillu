f1aa8e4cfbced3e9b476e367a110828903b2f053
Rajabaviri <92776535+Rajabaviri@users.noreply.github.com>
2022-03-03 21:11:24 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,6 @@
 public class Helloworld{
 public static void main(String[] args){
-	System.out.println("Hello Raja, welcome to jenkins world");
+	System.out.println("Hello Raja, atl");
 	int a=40;
 	System.out.println(a);
 }
